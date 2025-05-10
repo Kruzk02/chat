27562f1df8adf4308bbce5853ab0239424932a67
Thien phuc <thienphuc484@gmail.com>
2025-05-10 14:03:37 +0700

    feat(Server): add multiple thread

--- src/main/java/org/server/Server.java
+++ src/main/java/org/server/Server.java
@@ -3,33 +3,53 @@ package org.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 4);
 
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        String str = null;
-        while ((str = in.readLine()) != null) {
-            if (str.equals("exit")) {
-                out.println("good bye");
-                break;
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                pool.execute(new ClientHandler(clientSocket));
             }
-            out.println(str);
+        } catch (SocketException e) {
+            System.out.println("Server socket closed.");
         }
     }
 
     public void stop() throws IOException {
         serverSocket.close();
-        clientSocket.close();
-        out.close();
-        in.close();
+    }
+
+    private record ClientHandler(Socket clientSocket) implements Runnable {
+
+        @Override
+        public void run() {
+            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                String input;
+                while ((input = in.readLine()) != null) {
+                    if (input.equals("exit")) {
+                        out.println("good bye");
+                        break;
+                    }
+                    out.println(input);
+                }
+            } catch (IOException e) {
+                System.err.println("Client connection error: " + e.getMessage());
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
     }
 }
