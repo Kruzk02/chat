003d65e1b2dcd1c8ab9beaf231268f9080534c02
Thien phuc <thienphuc484@gmail.com>
2025-05-13 13:33:12 +0700

    feat(server): add sign a username

--- src/main/java/org/server/Server.java
+++ src/main/java/org/server/Server.java
@@ -5,7 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,7 +16,7 @@ public class Server {
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
 
-        Map<String, Set<PrintWriter>> groups = new ConcurrentHashMap<>();
+        Map<String, Map<String, PrintWriter>> groups = new ConcurrentHashMap<>();
 
         try {
             while (!serverSocket.isClosed()) {
@@ -33,7 +32,7 @@ public class Server {
         serverSocket.close();
     }
 
-    private record ClientHandler(Socket clientSocket, Map<String, Set<PrintWriter>> groups) implements Runnable {
+    private record ClientHandler(Socket clientSocket, Map<String, Map<String, PrintWriter>>  groups) implements Runnable {
 
         @Override
         public void run() {
@@ -42,18 +41,37 @@ public class Server {
                  BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
                 String currentGroup = null;
+                String username = null;
                 String input;
 
                 while ((input = in.readLine()) != null) {
-                    if (input.startsWith("JOIN ")) {
+                    if (input.startsWith("USERNAME ")) {
+                        username = input.substring(9);
+                        out.println("Logged in as: " + username);
+                    } else if (input.startsWith("JOIN ")) {
+                        if (username == null) {
+                            out.println("You must sign username first");
+                            continue;
+                        }
+
                         currentGroup = input.substring(5);
-                        groups.putIfAbsent(currentGroup, ConcurrentHashMap.newKeySet());
-                        groups.get(currentGroup).add(out);
+
+                        groups.putIfAbsent(currentGroup, new ConcurrentHashMap<>());
+                        groups.get(currentGroup).put(username, out);
+
                         out.println("Joined group: " + currentGroup);
                     } else if (input.startsWith("MESSAGE ") && currentGroup != null) {
                         String message = input.substring(8);
-                        for (var writer : groups.get(currentGroup)) {
-                            writer.println("[" + currentGroup + "]: " + message);
+                        for (var writer : groups.get(currentGroup).values()) {
+                            if (!writer.equals(out)) {
+                                writer.println(username + " has left the group.");
+                            }
+
+                            for (var entry : groups.get(currentGroup).entrySet()) {
+                                if (!entry.getKey().equals(username)) {
+                                    writer.println("[" + currentGroup + "] " + username + ": " + message);
+                                }
+                            }
                         }
                     } else if (input.equals("exit")){
                         out.println("Good Bye");
@@ -64,9 +82,13 @@ public class Server {
                 }
 
 
-                if (currentGroup != null) {
+                if (username != null && currentGroup != null) {
                     var group = groups.get(currentGroup);
-                    group.remove(out);
+
+                    var writer = group.remove(username);
+                    if (writer != null) {
+                        writer.close();
+                    }
 
                     if (group.isEmpty()) {
                         groups.remove(currentGroup);
