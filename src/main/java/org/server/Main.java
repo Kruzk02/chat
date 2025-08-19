a50215a11d9a6c34f02c9b30235a25091dd88715
Thien phuc <thienphuc484@gmail.com>
2025-08-19 10:56:56 +0700

    feat: add jdbc sqlite

--- src/main/java/org/server/Main.java
+++ src/main/java/org/server/Main.java
@@ -1,10 +1,20 @@
 package org.server;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Server server = new Server();
-        server.start(8080);
+      var url = "jdbc:sqlite:db/chat.db";
+
+      try (var conn = DriverManager.getConnection(url)) {
+        System.out.println("Connection to SQLite has been established.");
+      } catch (SQLException e) {
+        System.out.println(e.getMessage());
+      }
+
+      Server server = new Server();
+      server.start(8080);
     }
 }
