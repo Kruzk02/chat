9a7e599761964c1c4d2305914e013afadb62c07c
Thien phuc <thienphuc484@gmail.com>
2025-09-26 09:09:29 +0700

    add command U login and register

--- src/main/java/org/client/Main.java
+++ src/main/java/org/client/Main.java
@@ -11,8 +11,44 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     while (true) {
       String str = scanner.nextLine();
+      if (str.equals("U LOGIN")) {
 
-      client.send(str);
+        System.out.println("Enter your username: ");
+        String username = scanner.nextLine();
+        if (username.contains(" ")) {
+          System.out.println("Username shouldn't contain space.");
+          return;
+        }
+
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+        if (password.contains(" ")) {
+          System.out.println("Password shouldn't contain space.");
+          return;
+        }
+
+        client.send(str + " " + username + " " + password);
+      } else if (str.equals("U REGISTER")) {
+
+        System.out.println("Enter your username: ");
+        String username = scanner.nextLine();
+        if (username.contains(" ")) {
+          System.out.println("Username shouldn't contain space.");
+          return;
+        }
+
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+        if (password.contains(" ")) {
+          System.out.println("Password shouldn't contain space.");
+          return;
+        }
+
+        client.send(str + " " + username + " " + password);
+      } else {
+        client.send(str);
+      }
+
 
       if (str.equals("E")) {
         client.stop();
